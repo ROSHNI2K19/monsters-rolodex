51de079c57a04f71f09c69a46af0952ac4cceb92
ROSHNI2K19 <letters2roshni@gmail.com>
2019-08-02 15:05:24 +0200

    Updates

---
+++ precache-manifest.fbe2023e0403187acb1561a157edaea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3c36a24842d5a5a47b6e5362618993",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe8944b00e3960ada3fe",
+    "url": "/monsters-rolodex/static/css/main.66c00379.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fe8944b00e3960ada3fe",
+    "url": "/monsters-rolodex/static/js/main.71f4b556.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
